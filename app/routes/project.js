e92831105a4960b5bbcbb38729aea3924faac91b
J. Ryan Conklin <jryanconklin@gmail.com>
2016-11-08 15:57:09 -0800

    Add Console Log to Project

--- app/routes/project.js
+++ app/routes/project.js
@@ -15,10 +15,22 @@ export default Ember.Route.extend({
   actions: {
 
     buildSVG(model) {
-      // console.log( parseInt(model[0].usd_pledged) );
-      var chart = d3.select("#chart");
-      var div = chart.append('div'); 
-      div.html(model[0].blurb);
+      console.log( parseInt(model[0].usd_pledged) );
+
+      var sampleSVG = d3.select("#chart")
+              .append("svg")
+              .attr("width", 100)
+              .attr("height", 100);
+
+          sampleSVG.append("circle")
+              .style("stroke", "gray")
+              .style("fill", "white")
+              .attr("r", 40)
+              .attr("cx", 50)
+              .attr("cy", 50)
+              .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
+              .on("mouseout", function(){d3.select(this).style("fill", "white");});
+
 
 
     },
