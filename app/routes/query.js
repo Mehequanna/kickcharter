e3e431e8b5436df14c33b0c5e2fd53e7155c6cfd
Caleb Paul <calebpaul21@gmail.com>
2016-11-07 16:50:20 -0800

    add more boilerplate concept properties

--- app/routes/query.js
+++ app/routes/query.js
@@ -1,4 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model: function(params) {
+    var goal = ""; //NUMBER
+    var pledged = ""; //NUMBER
+    var currency = "";
+    var currency_symbol = "";
+    var state = "";
+    var photo = "";
+    var name = "";
+    var blurb = "";
+    var deadline = ""; //NUMBER
+    var created_at = ""; //NUMBER
+    var launched_at = ""; //NUMBER
+    var locationShortName = ""; //Property of
+
+    var url = 'https://www.kickstarter.com/projects/search.json?search=&term=Tabletop%20Games';
+    return Ember.$.getJSON(url).then(function(responseJSON) {
+      birthday = responseJSON.results[0].birthday;
+
+      var returnArray = {'legislators': responseJSON.results, 'birthday': birthday};
+
+      return returnArray;
+   });
+  }
+
 });
