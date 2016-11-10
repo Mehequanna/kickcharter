813aed16be6fa59f552ff6bc6f4858aff563eee5
Stephen Emery <stephen@>
2016-11-10 08:26:23 -0800

    Add some styling.

--- app/components/feature-view.js
+++ app/components/feature-view.js
@@ -5,7 +5,6 @@ export default Ember.Component.extend({
 
   didRender() {
     this._super();
-    this._actions['testaction'];
     console.log("init hoook");
     // this.buildSVG('model');
 
@@ -36,7 +35,7 @@ export default Ember.Component.extend({
           .attr("width", w)
           .attr("height", h);
 
-    var text = d3.select("#chart").append("p").text("Goal: " + this.get('model').goal + " " + "Pledged: " + this.get('model').usd_pledged)
+    var text = d3.select("#chart").append("p").text("Goal: $" + this.get('model').goal.toFixed(2) + " " + "Pledged: $" + this.get('model').pledged.toFixed(2))
           .append("rect")
 
     svg.selectAll("rect")
@@ -66,64 +65,6 @@ export default Ember.Component.extend({
 
   },
   actions: {
-    testaction(){
-      console.log("this is a test");
-    },
-    // buildSVG(model) {
-    //   console.log( parseFloat(model.usd_pledged) );
-    //   d3.select("#chart").clear();
-    //   //width and height
-    //   var w = 200;
-    //   var h = 100;
-    //   var barPadding = 10;
-    //
-    //   var pledged = parseInt(model.pledged * model.static_usd_rate);
-    //   var goal = parseInt(model.goal * model.static_usd_rate);
-    //
-    //   var unFunded = (pledged / goal) * 100;
-    //   var funded = (goal / pledged) * 100;
-    //
-    //   var dataset = [ 50, 50];
-    //
-    //   //sets high number as 100(equal to div height), low number as % of high number
-    //   if (pledged >= goal) {
-    //      dataset = [funded, 100];
-    //   } else if (goal > pledged) {
-    //     dataset = [100, unfunded];
-    //   }
-    //
-    //   var svg = d3.select("#chart")
-    //         .append("svg")
-    //         .attr("width", w)
-    //         .attr("height", h);
-    //
-    //   var text = d3.select("#chart").append("p").text("Goal: " + model.goal + " " + "Pledged: " + model.usd_pledged)
-    //         .append("rect")
-    //
-    //   svg.selectAll("rect")
-    //     .data(dataset)
-    //     .enter()
-    //     .append("rect")
-    //     .attr("x", function(d, i) {
-    //       return i * 5;  //Bar width of 20 plus 1 for padding
-    //     })
-    //     .attr("x", function(d, i) {
-    //       return i * (w / dataset.length);
-    //     })
-    //     .attr("width", w / dataset.length - barPadding)
-    //     .attr("height", function(d) {
-    //       return d * 4;
-    //     })
-    //     .attr("y", function(d) {
-    //       return h - d;  //Height minus data value
-    //     })
-    //     .attr("height", function(d) {
-    //       return d;  //Just the data value
-    //     })
-    //     .attr("fill", function(d) {
-    //       return "rgb(0, 0, " + (d * 10) + ")";
-    //     });
-    //
-    // },
+
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,9 +3,9 @@ import d3 from 'd3';
 
 export default Ember.Route.extend({
   model: function() {
-    var url = 'https://www.kickstarter.com/projects/search.json?term=Tabletop&sort=end_date&per_page=40';
+    var url = 'https://www.kickstarter.com/projects/search.json?term=Tabletop&per_page=10';
     return Ember.$.getJSON(url).then(function(responseJSON) {
-      return responseJSON.projects[4];
+      return responseJSON.projects[9];
     });
   },
 
